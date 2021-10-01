c54d92973a28d28d04e34233db604a89d6c702e1
Eric Fritz <eric@eric-fritz.com>
2021-10-01 12:33:41 -0500

    add default region

--- .buildkite/terraform-validate.sh
+++ .buildkite/terraform-validate.sh
@@ -5,10 +5,14 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
 MODULES=(
-  modules/docker-mirror
-  modules/executors
-  modules/networking
+  ./modules/networking
+  ./modules/docker-mirror
+  ./modules/executors
 )
+
+# Ensure terraform validate has a valid region
+# https://github.com/hashicorp/terraform/issues/21408#issuecomment-495746582
+export AWS_DEFAULT_REGION=us-east-2
 
 for module in "${MODULES[@]}"; do
   pushd "${module}"
