534bc8bec655fbf8d3322bce8a01ad8f9333411b
Erik Seliger <erikseliger@me.com>
2021-10-07 16:47:03 +0200

    Revert

--- examples/single-executor/main.tf
+++ examples/single-executor/main.tf
@@ -12,6 +12,7 @@ module "executors" {
   docker_mirror_static_ip                      = local.docker_mirror_static_ip
   executor_instance_tag                        = "codeintel-prod"
   executor_sourcegraph_external_url            = "https://sourcegraph.acme.com"
+  executor_sourcegraph_executor_proxy_username = "executor"
   executor_sourcegraph_executor_proxy_password = "hunter2"
   executor_queue_name                          = "codeintel"
   executor_metrics_environment_label           = "prod"
