6e3d8e3d0fb9b544a65688ca78bccba72b1e145d
Erik Seliger <erikseliger@me.com>
2021-10-07 16:55:56 +0200

    Update and fix examples

--- examples/single-executor/main.tf
+++ examples/single-executor/main.tf
@@ -6,13 +6,12 @@ locals {
 
 module "executors" {
   source  = "sourcegraph/executors/aws"
-  version = "0.0.9"
+  version = "0.0.12"
 
   availability_zone                            = local.availability_zone
   docker_mirror_static_ip                      = local.docker_mirror_static_ip
   executor_instance_tag                        = "codeintel-prod"
   executor_sourcegraph_external_url            = "https://sourcegraph.acme.com"
-  executor_sourcegraph_executor_proxy_username = "executor"
   executor_sourcegraph_executor_proxy_password = "hunter2"
   executor_queue_name                          = "codeintel"
   executor_metrics_environment_label           = "prod"
