0a977a27d928cf93eec1eeceb753de543521fd64
Erik Seliger <erikseliger@me.com>
2021-10-26 23:18:17 +0200

    Fix image tag

--- modules/docker-mirror/variables.tf
+++ modules/docker-mirror/variables.tf
@@ -10,7 +10,7 @@ variable "subnet_id" {
 
 variable "machine_ami" {
   type        = string
-  default     = "ami-04010bfdd784ec00e"
+  default     = "ami-09c5b27afc570bedf"
   description = "AMI for the EC2 instance to use. Must be in the same availability zone."
 }
 
