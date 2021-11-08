e006edae35a9e2bbc8345a279b9887aa4649d5b6
Erik Seliger <erikseliger@me.com>
2021-11-08 22:52:33 +0100

    Add sudo to startup script

--- modules/docker-mirror/startup-script.sh
+++ modules/docker-mirror/startup-script.sh
@@ -5,24 +5,24 @@ set -euxo pipefail
 # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/device_naming.html#available-ec2-device-names
 VOLUMES_NAME="$(find /dev -maxdepth 1 | grep -i 'nvme[0-21]n1$')"
 for VOLUME in ${VOLUMES_NAME}; do
-  ALIAS=$(nvme id-ctrl -H -v "${VOLUME}" | grep -Po '/dev/(sd[b-z]|xvd[b-z])')
+  ALIAS=$(sudo nvme id-ctrl -H -v "${VOLUME}" | grep -Po '/dev/(sd[b-z]|xvd[b-z])')
   if [ -n "${ALIAS}" ]; then
-    ln -s "${VOLUME}" "${ALIAS}"
+    sudo ln -s "${VOLUME}" "${ALIAS}"
   fi
 done
 
 REALPATH="$(realpath /dev/sdh)"
 # Ensure /dev/sdh has a file system.
 if [ "$(file -s "$REALPATH")" == "$REALPATH: data" ]; then
-  mkfs.ext4 /dev/sdh
+  sudo mkfs.ext4 /dev/sdh
 fi
 
 # Mount /dev/sdh to /mnt/registry.
-mkdir -p /mnt/registry
-echo "/dev/sdh /mnt/registry ext4 defaults 0 2" >>/etc/fstab
-mount -a
-chown ubuntu:ubuntu /mnt/registry
+sudo mkdir -p /mnt/registry
+echo "/dev/sdh /mnt/registry ext4 defaults 0 2" | sudo tee -a /etc/fstab
+sudo mount -a
+sudo chown ubuntu:ubuntu /mnt/registry
 
 # Enable and start the registry service.
-systemctl enable docker_registry
-systemctl start docker_registry
+sudo systemctl enable docker_registry
+sudo systemctl start docker_registry
