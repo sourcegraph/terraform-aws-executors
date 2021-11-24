0f630d568e53188e83d0f9e7b274fc5edd2f99e0
Chris Wendt <chrismwendt@gmail.com>
2021-11-23 17:42:45 -0700

    Check markdown link versions (#20)

--- .buildkite/check-latest-tag.sh
+++ .buildkite/check-latest-tag.sh
@@ -6,7 +6,7 @@ function finish() {
 }
 trap finish EXIT
 
-set -ex
+set -e -o pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
@@ -23,11 +23,30 @@ get_latest() {
 
 latest="$(get_latest)"
 
-git grep --line-number "# LATEST" examples >"$scratch/want.txt" || true
-git grep --line-number "\"$latest\" # LATEST" examples >"$scratch/got.txt" || true
+check_versions() {
+  file_pattern="$1"
+  all_regex="$2"
+  correct_regex="$3"
+  find . -name "$file_pattern" -print0 | xargs -0 grep -n "$all_regex" >"$scratch/all.txt" || true
+  find . -name "$file_pattern" -print0 | xargs -0 grep -n "$correct_regex" >"$scratch/correct.txt" || true
+  if ! (git diff --color -U0 --no-index --exit-code "$scratch/all.txt" "$scratch/correct.txt" | tail -n +6); then
+    echo ""
+    echo "❌ Detected out of sync versions in the lines above (should be version $latest)"
+    exit 1
+  fi
+}
 
-if ! git diff --no-index --exit-code "$scratch/want.txt" "$scratch/got.txt"; then
-  echo ""
-  echo "❌ Detected old versions! Make sure that all versions \`version = \"...\" # LATEST\` match the latest git tag: $latest"
-  exit 1
-fi
+check_versions \
+  "*.tf" \
+  "# LATEST" \
+  "\"$latest\" # LATEST"
+
+check_versions \
+  "*.md" \
+  "https://registry.terraform.io/modules/sourcegraph/executors/aws/[0-9]\+\.[0-9]\+\.[0-9]\+" \
+  "https://registry.terraform.io/modules/sourcegraph/executors/aws/$latest"
+
+check_versions \
+  "*.md" \
+  "https://github.com/sourcegraph/terraform-aws-executors/blob/v[0-9]\+\.[0-9]\+\.[0-9]\+" \
+  "https://github.com/sourcegraph/terraform-aws-executors/blob/v$latest"
--- modules/networking/README.md
+++ modules/networking/README.md
@@ -1,5 +1,5 @@
 # Networking module
 
-This module provides the networking glue between the sibling [executors](https://registry.terraform.io/modules/sourcegraph/executors/aws/latest/submodules/executors) and [docker-mirror](https://registry.terraform.io/modules/sourcegraph/executors/aws/latest/submodules/docker-mirror) modules.
+This module provides the networking glue between the sibling [executors](https://registry.terraform.io/modules/sourcegraph/executors/aws/0.0.21/submodules/executors) and [docker-mirror](https://registry.terraform.io/modules/sourcegraph/executors/aws/0.0.21/submodules/docker-mirror) modules.
 
 _(There's very little to talk about in here.)_
