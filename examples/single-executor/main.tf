24799a7dd63770c3f77c9df30afe35de507faa42
Erik Seliger <erikseliger@me.com>
2022-02-17 14:58:51 +0100

    Align module releases and Sourcegraph releases
    And add some documentation on compatibility to the readme.

--- examples/single-executor/main.tf
+++ examples/single-executor/main.tf
@@ -5,7 +5,7 @@ locals {
 
 module "executors" {
   source  = "sourcegraph/executors/aws"
-  version = "0.0.27" # LATEST
+  version = "3.37.0" # LATEST
 
   availability_zone                            = local.availability_zone
   executor_instance_tag                        = "codeintel-prod"
