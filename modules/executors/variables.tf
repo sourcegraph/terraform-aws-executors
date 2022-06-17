1c7aa0f4545c06c6f359c0c83e40262e11214105
Erik Seliger <erikseliger@me.com>
2022-06-17 14:36:55 +0200

    Bump for 3.41 release (#35)

--- modules/executors/variables.tf
+++ modules/executors/variables.tf
@@ -16,7 +16,7 @@ variable "resource_prefix" {
 
 variable "machine_image" {
   type        = string
-  default     = "ami-051a5bdea41dd7493"
+  default     = "ami-0c78e1e42f2133321"
   description = "Executor node machine disk image to use for creating the boot volume."
 }
 
