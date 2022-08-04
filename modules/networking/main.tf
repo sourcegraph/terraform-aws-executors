7819d5dfa12fc5dea44a839b69487e62e88002cd
Erik Seliger <erikseliger@me.com>
2022-08-05 01:06:13 +0200

    Fixup

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -6,13 +6,13 @@ locals {
 # We will have a subnet in there, that has an all-destination route to an egress-only
 # internet gateway. That way, we protect from incoming traffic.
 resource "aws_vpc" "default" {
-  cidr_block                       = local.ip_cidr
+  cidr_block                       = "10.0.0.0/16"
   assign_generated_ipv6_cidr_block = true
 }
 
 resource "aws_subnet" "default" {
   vpc_id            = aws_vpc.default.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = local.ip_cidr
   availability_zone = var.availability_zone
 }
 
