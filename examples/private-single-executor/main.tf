a62b20b001b32a80c9ead19ce7189be64ff2a354
Erik Seliger <erikseliger@me.com>
2022-08-07 15:38:08 +0200

    Example

---
+++ examples/private-single-executor/main.tf
@@ -0,0 +1,18 @@
+locals {
+  region            = "us-west-2"
+  availability_zone = "us-west-2a"
+}
+
+module "executors" {
+  source  = "sourcegraph/executors/aws"
+  version = "3.42.0" # LATEST
+
+  availability_zone                            = local.availability_zone
+  executor_instance_tag                        = "codeintel-prod"
+  executor_sourcegraph_external_url            = "https://sourcegraph.acme.com"
+  executor_sourcegraph_executor_proxy_password = "hunter2"
+  executor_queue_name                          = "codeintel"
+  executor_metrics_environment_label           = "prod"
+  executor_use_firecracker                     = true
+  #   private_networking                           = true
+}
