6b9bfd15412adaeb137d346d0ebafd85641f891f
Erik Seliger <erikseliger@me.com>
2022-08-07 22:26:58 +0200

    Prepare release for 3.42.1 module version (#43)

--- examples/private-single-executor/main.tf
+++ examples/private-single-executor/main.tf
@@ -5,7 +5,7 @@ locals {
 
 module "executors" {
   source  = "sourcegraph/executors/aws"
-  version = "3.42.0" # LATEST
+  version = "3.42.1" # LATEST
 
   availability_zone                            = local.availability_zone
   executor_instance_tag                        = "codeintel-prod"
