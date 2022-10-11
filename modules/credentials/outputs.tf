419357b71fe82f14331f5b8b931b01dbf4612235
Randell Callahan <piszmogcode@gmail.com>
2022-10-11 08:35:05 -0600

    Add description to outputs

--- modules/credentials/outputs.tf
+++ modules/credentials/outputs.tf
@@ -1,7 +1,9 @@
 output "metric_writer_access_key_id" {
-  value = aws_iam_access_key.metric_writer.id
+  value       = aws_iam_access_key.metric_writer.id
+  description = "The ID of the Metrics Writer Service Account."
 }
 
 output "metric_writer_secret_key" {
-  value = aws_iam_access_key.metric_writer.secret
+  value       = aws_iam_access_key.metric_writer.secret
+  description = "The secret key of the Metrics Writer Service Account."
 }
