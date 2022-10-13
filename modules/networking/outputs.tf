aa5dc958e6ff6f41db265562a6e2a3aa2d44ca7d
Randell Callahan <piszmogcode@gmail.com>
2022-10-13 08:21:22 -0600

    Fix descriptions to be more clear

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -5,12 +5,12 @@ output "vpc_id" {
 
 output "subnet_id" {
   value       = var.nat == true ? aws_subnet.private.0.id : aws_subnet.default.id
-  description = "The subnet to run the VM in."
+  description = "The subnet to run the VMs in."
 }
 
 output "ip_cidr" {
   value       = local.ip_cidr
-  description = "The internal address that is owned by the subnetwork."
+  description = "The CIDR range of the subnet in which the executor machines will be spawned."
 }
 
 output "nat_ip" {
