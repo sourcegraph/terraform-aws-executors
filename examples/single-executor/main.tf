18ee66e2de52c38013f3c9368527ee6f656b86fc
Sander Ginn <sanderginn@users.noreply.github.com>
2023-06-29 15:01:19 +0200

    Update files for 5.1.1 release (#84)

--- examples/single-executor/main.tf
+++ examples/single-executor/main.tf
@@ -5,7 +5,7 @@ locals {
 
 module "executors" {
   source  = "sourcegraph/executors/aws"
-  version = "5.1.0" # LATEST
+  version = "5.1.1" # LATEST
 
   availability_zone                            = local.availability_zone
   executor_instance_tag                        = "codeintel-prod"
