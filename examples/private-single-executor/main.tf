3b8d279238db971f074c22c7d19e9cd8a0aff640
Anish Lakhwara <anish+git@lakhwara.com>
2024-09-04 22:32:41 +0000

    release_minor: v5.7.0
    {"version":"v5.7.0","inputs":"server=v5.7.0","type":"minor"}

--- examples/private-single-executor/main.tf
+++ examples/private-single-executor/main.tf
@@ -5,7 +5,7 @@ locals {
 
 module "executors" {
   source  = "sourcegraph/executors/aws"
-  version = "5.6.0" # LATEST
+  version = "5.7.0" # LATEST
 
   availability_zone                            = local.availability_zone
   executor_instance_tag                        = "codeintel-prod"
