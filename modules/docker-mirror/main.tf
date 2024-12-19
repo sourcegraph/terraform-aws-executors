09ad4648a60e1d37cffa092f36c3dbe061856f9e
Jacob Pleiness <jacob.pleiness@sourcegraph.com>
2024-12-19 16:25:54 +0000

    release_minor: v5.11.0
    {"version":"v5.11.0","inputs":"server=5.11.0","type":"minor"}

--- modules/docker-mirror/main.tf
+++ modules/docker-mirror/main.tf
@@ -48,7 +48,7 @@ data "aws_ami" "latest_ami" {
 
   filter {
     name   = "name"
-    values = ["sourcegraph-executors-docker-mirror-5-10-*"]
+    values = ["sourcegraph-executors-docker-mirror-5-11-*"]
   }
 
   filter {
