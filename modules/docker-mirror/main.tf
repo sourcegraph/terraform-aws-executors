2737f52e8440cd5215eb5b6705274d2d51864678
Jacob Pleiness <jacob.pleiness@sourcegraph.com>
2025-02-17 18:22:11 -0500

    chore(rel): update for with 6.1.0 release

--- modules/docker-mirror/main.tf
+++ modules/docker-mirror/main.tf
@@ -48,7 +48,7 @@ data "aws_ami" "latest_ami" {
 
   filter {
     name   = "name"
-    values = ["sourcegraph-executors-docker-mirror-6-0-*"]
+    values = ["sourcegraph-executors-docker-mirror-6-1-*"]
   }
 
   filter {
