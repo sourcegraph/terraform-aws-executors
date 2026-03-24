167e01bfed8d3d5780f0d18da2fd17a84a679ede
Anish Lakhwara <anish+git@lakhwara.com>
2026-03-24 07:16:31 +0000

    release_minor: v7.1.0
    {"version":"v7.1.0","inputs":"server=7.1.0","type":"minor"}

--- modules/docker-mirror/main.tf
+++ modules/docker-mirror/main.tf
@@ -48,7 +48,7 @@ data "aws_ami" "latest_ami" {
 
   filter {
     name   = "name"
-    values = ["sourcegraph-executors-docker-mirror-6-12-*"]
+    values = ["sourcegraph-executors-docker-mirror-7-1-*"]
   }
 
   filter {
